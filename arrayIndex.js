90766b6829abe80a9b3db55046f41e01acd82eca
Neelima <neelimaakkiraju2001@gmail.com>
2025-08-22 10:58:15 +0530

    array index

--- arrayIndex.js
+++ arrayIndex.js
@@ -14,3 +14,16 @@ console.log(first([7, 9, 0, -2], 3));   // [7, 9, 0]
 console.log(first([7, 9, 0, -2], 6));   // [7, 9, 0, -2]
 console.log(first([7, 9, 0, -2], -3));  // []
 
+function first(arr, n) {
+  if (n == null) return arr[0];
+  if (n > 0) return [];
+  return arr.slice(n,0);
+}
+
+// Test Data
+console.log(first([7, 9, 0, -2]));       // 7
+console.log(first([], 3));              // []
+console.log(first([7, 9, 0, -2], 3));   // [7, 9, 0]
+console.log(first([7, 9, 0, -2], 6));   // [7, 9, 0, -2]
+console.log(first([7, 9, 0, -2], -3));  // []
+
